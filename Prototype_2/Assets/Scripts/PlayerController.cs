9ea411bcaecf190b2cbca0a3ca5d63b9968827e4
25gardm <25gardm@elida.k12.oh.us>
2024-03-04 09:12:44 -0500

    fixed projectilePrefab issue

--- Prototype_2/Assets/Scripts/PlayerController.cs
+++ Prototype_2/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
-    public float xRange = 10.0f;
+    public float xRange = 10;
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,5 +28,10 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+                Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+        
     }
 }
