b6a4ddade7455c7b956fa7d1a365b1b66d50edca
25gardm <25gardm@elida.k12.oh.us>
2024-03-15 09:00:01 -0400

    finished challenge 2

---
+++ Prototype_2/Assets/Challenge 2/Scripts/MoveForwardX.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForwardX : MonoBehaviour
+{
+    public float speed;
+    public float timeDelay = 3.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * timeDelay);
+    }
+}
