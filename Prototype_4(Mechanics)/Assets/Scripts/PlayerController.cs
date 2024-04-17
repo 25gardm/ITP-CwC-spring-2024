9be8500f6d447d4aa089e06e819279e9fcfc190f
25gardm <25gardm@elida.k12.oh.us>
2024-04-17 09:13:27 -0400

    4.3 step 3 finished

--- Prototype_4(Mechanics)/Assets/Scripts/PlayerController.cs
+++ Prototype_4(Mechanics)/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float speed = 5.0f;
     private GameObject focalPoint;
+    public bool hasPowerup;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +20,20 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Debug.Log("Collided with" + collision.gameObject.name + " with Powerup set to " + hasPowerup);
+    }
     }
 }
