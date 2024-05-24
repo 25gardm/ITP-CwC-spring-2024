dc6331c7c78ee67af020d65fc5eeac82da71f948
25gardm <25gardm@elida.k12.oh.us>
2024-05-24 09:02:01 -0400

    final stuff

--- Final(sticks)/Assets/Scripts/PlayerController.cs
+++ Final(sticks)/Assets/Scripts/PlayerController.cs
@@ -6,16 +6,28 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     public float speed = 5.0f;
+    private GameObject focalPoint;
+    private bool hasStick;
     // Start is called before the first frame update
     void Start()
     {
-        playerRb = GetComponent<Rigidbody>();  
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
     }
 
     // Update is called once per frame
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(Vector3.forward * speed * forwardInput); 
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput); 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Stick"))
+        {
+            hasStick = true;
+            Destroy(other.gameObject);
+        }
     }
 }
